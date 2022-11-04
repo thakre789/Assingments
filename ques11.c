058dd57d8e7a6fd95febee90975a0a98d9fbcbf3
unknown <sachinthakre78@gmail.com>
2022-11-04 10:54:31 +0530

    v1.0

--- ques11.c
+++ ques11.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
 int main()
-{  float INR;
+
+{ 
+   float INR;
    printf("enter the amount in INR");
    scanf("%f",&INR);
    printf("the amount in USD is %f",INR/76.23);
    return 0;
+   
 }
